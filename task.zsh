53ef408cde1fb7541031908e1845a6c1ffa8b326
connero <88785126+conneroisu@users.noreply.github.com>
2024-06-05 16:11:12 -0500

    latest

--- task.zsh
+++ task.zsh
@@ -3,13 +3,13 @@ local state line
 declare -A opt_args
 
 _task_get_tasks () {
-  local file=$1
-  sed '1,/tasks:/d' "$file" | grep -iwE "^\ \ [a-z](.*)$" | sed 's/.$//' | awk '{ print $1 }'
+  sed '1,/tasks:/d' Taskfile.yaml | grep -iwE "^\ \ [a-z](.*)$" | sed 's/.$//' | awk '{ print $1 }'
   while read included_task
   do
-    taskfile_path=$(sed "1,/$included_task:/d" "$file" | grep -m 1 -iwE "^\  \ \ taskfile: .*$" | awk '{ print $2 }')
-    sed '1,/tasks:/d' "$taskfile_path" | grep -iwE "^\ \ [a-z](.*)$" | sed 's/.$//' | awk -v included_task="$included_task" '{ print included_task":"$1 }'
-  done < <(sed '1,/includes:/d' "$file" | grep -iwE "^\ \ [a-z](.*)$" | sed 's/.$//' | awk '{ print $1 }')
+    taskfile_path=$(sed "1,/$included_task:/d" Taskfile.yaml | grep -m 1 -iwE "^\  \ \ taskfile: .*$" | awk '{ print $2 }')
+    sed '1,/tasks:/d' $taskfile_path | grep -iwE "^\ \ [a-z](.*)$" | sed 's/.$//' | awk -v included_task="$included_task" '{ print included_task":"$1 }'
+    
+  done < <(sed '1,/includes:/d' Taskfile.yaml | grep -iwE "^\ \ [a-z](.*)$" | sed 's/.$//' | awk '{ print $1 }')
 }
 
 _task () {
@@ -19,13 +19,10 @@ _task () {
   _arguments \
     '*: :->args'
   if [ -f Taskfile.yaml ]; then
-    compadd `_task_get_tasks "Taskfile.yaml"`
-  elif [ -f Taskfile.yml ]; then
-    compadd `_task_get_tasks "Taskfile.yml"`
+    compadd `_task_get_tasks`
   else
-    echo -e "\nNo taskfile found"
+   echo -e "\nNo taskfile found"
   fi
 }
 
 compdef _task task
-autoload -Uz bashcompinit && bashcompinit
